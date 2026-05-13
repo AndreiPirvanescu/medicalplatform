b80b41779bffa126758d666cb3392bd5a393c9fb
Andrei Pirvanescu <andrei_parvanescu@yahoo.com>
2026-05-14 00:42:51 +0300

    First commit

---
+++ src/main/java/com/andrei/project/medicalplatform/MedicalPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.andrei.project.medicalplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andrei/project/medicalplatform/MedicalPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrei.project.medicalplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
